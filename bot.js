ecbaa9f45178699a2795c97875c064e76a661c4f
Mr DaHoM <44880953+mrdahom4@users.noreply.github.com>
2018-12-07 15:31:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,4 +37,18 @@ client.user.setGame(`iLove Simo`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("MANAGE ROLES"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
 client.login(process.env.BOT_TOKEN);
